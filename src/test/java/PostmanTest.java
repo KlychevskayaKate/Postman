8a4e21a06c2793cf9ed8abb2420bf34608f14be2
Klychevskaya Kate <kateklych@gmail.com>
2022-10-07 11:10:24 +0300

    new test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -17,7 +17,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo ("somedata"))
+                .body("data", equalTo ("some data"))
         ;
     }
 }
